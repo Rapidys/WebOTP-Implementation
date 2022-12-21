b13d8bb0671476ff66e3b9bd173d8845d492ecbc
Rapidys <ladodolidze2000@gmail.com>
2022-12-22 00:43:09 +0400

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 
-export default class App extends React.Component {
-    state = {
-        otp: "",
-    };
+const App = () => {
 
-    componentDidMount() {
+    const [otp, setOTP] = useState('')
+    const inputRef = useRef()
+
+
+    useEffect(() => {
         if ("OTPCredential" in window) {
             const ac = new AbortController();
 
             navigator.credentials
                 .get({
-                    otp: { transport: ["sms"] },
+                    otp: {transport: ["sms"]},
                     signal: ac.signal,
                 })
                 .then((otp) => {
-                    this.setState({ otp: otp.code });
+                    inputRef.current.value = otp.code
                     ac.abort();
                 })
                 .catch((err) => {
@@ -23,39 +24,43 @@ export default class App extends React.Component {
                     console.log(err);
                 });
         }
-    }
+    }, [])
 
-    render() {
-        return (
-            <div className="App">
-                <input type="text" autocomplete="one-time-code"/>
-                <br />
-                <br />
-                <br />
-                <br />
-                <h2>Your OTP is: {this.state.otp}</h2>
-                <br />
-                <br />
-                <br />
-                <br />
-                <h3>The Web OTP API Docs</h3>
-                <div>
-                    Send an SMS that includes
-                    <pre>
+    useEffect(() => {
+        alert(otp)
+    }, [otp])
+
+    return (
+        <div className="App">
+            <input type="text" autocomplete="one-time-code" ref={inputRef} onChange={() => setOTP(inputRef.current.value)}/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h2>Your OTP is: {otp}</h2>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h3>The Web OTP API Docs</h3>
+            <div>
+                Send an SMS that includes
+                <pre>
             <code>@web-otp-shravan.netlify.app #12345</code>
           </pre>
-                    at the last line to this phone.
-                    <pre>
+                at the last line to this phone.
+                <pre>
             <code>please don't use https:// and / end of the url</code>
           </pre>
-                    <pre style={{ color: "green" }}>
+                <pre style={{color: "green"}}>
             <code>example for use @www.google.com</code>
           </pre>
-                    <pre style={{ color: "red" }}>
+                <pre style={{color: "red"}}>
             <code>example for not use XXXXX @https://www.google.com XXXXX</code>
           </pre>
-                </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+export default App
